a584523ec1ecf719773f96641c016043c50880c1
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-02-20 22:28:49 -0500

    Add javascript to home screen

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+$(".title-item").mouseenter(function(){
+    // console.log("mouse enter", this);
+    var letter = this.name;
+    this.src = "assets/alphabet/" + letter + "-outline.png";
+}).mouseleave(function(){
+    // console.log("mouse leave", this);
+    var newletter = this.name;
+    this.src = "assets/alphabet/" + newletter + "-full.png";
+});
