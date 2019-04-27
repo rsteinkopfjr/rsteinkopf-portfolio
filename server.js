8b052762afb66d7b5e0ab06fe9d0e3ae45ff3630
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-04-27 12:23:51 -0400

    Fix a lot

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
-
+// Dependencies
+// =============================================================
 var express = require("express");
+var path = require("path");
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
-
-// Tells node that we are creating an "express" server
+// Sets up the Express App
+// =============================================================
 var app = express();
-
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public/'));
+// Routes
+// =============================================================
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/index", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/about", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/about.html"));
+});
+app.get("/contact", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/contact.html"));
+});
+app.get("/skills", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/skills.html"));
+});
+app.get("/work", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/work.html"));
+});
 
-require("./routes/htmlRoutes")(app);
-
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
-
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
